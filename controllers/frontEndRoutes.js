43f3ad4ed8f335ea01d791a6a681f97636a388f3
Meghan Andrewartha <megandrew1918@gmail.com>
2021-08-23 14:09:43 -0700

    adding login routes

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,1 +1,44 @@
-// fronend routes
+onst express = require('express');
+const router = express.Router();
+const db = require('../models');
+const withAuth = require('../utils/auth');
+
+//get all
+
+//login
+router.get("/login", (req,res) =>{
+    console.log(req.session)
+      if(req.session.user){
+        res.redirect("/profile")  
+        return 
+      }
+      res.render("loginSignup")
+  })
+
+//logout
+router.get("/logout", (req,res)=>{
+    req.session.destroy();
+    res.redirect("/login");
+})
+
+//sign up
+router.get("/signUp", (req,res) =>{
+    if(req.session.user){
+        res.redirect("/profile")  
+        return 
+      }
+    res.render("createUser")
+})
+
+// Session route to check if logged in
+router.get("/session",(req,res)=>{
+    res.json({
+        sessionData:req.session
+        
+    })
+  })
+
+  //get profile by id
+
+  
+  module.exports = router
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,20 @@
-// index for controllers
+const express = require('express');
+const { route } = require('./api');
+const router = express.Router();
+const apiRoutes = require("./api")
+const frontEndRoutes = require("./frontEndRoutes")
+
+
+router.use("/api",apiRoutes)
+router.use("/", frontEndRoutes);
+
+
+
+router.get("/readsessions",(req,res)=>{
+    res.json({
+        sessions:req.session
+    })
+})
+
+
+module.exports = router;
