e0a76816b17fdbdb180af036db4786c5eb887f96
KeithMurph <keithmurphy94@gmail.com>
2021-09-02 11:12:31 -0700

    log miles api route working

--- src/components/logMiles/mileInput.js
+++ src/components/logMiles/mileInput.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import API from "../../utils/API";
 const initialFormnData = {
   formMiles: "",
 };
@@ -17,7 +18,7 @@ export default function MileInput(props) {
     e.preventDefault();
     const userMiles = 50;
     // const groupMiles = 50;
-    const userMilesToGo = 1900;
+    // const userMilesToGo = 1900;
     // const groupMilesToGo= 1900;
 
 
@@ -25,7 +26,7 @@ export default function MileInput(props) {
     // console log
     console.log("current user miles " + userMiles);
     // console.log("current group miles " + groupMiles);
-    console.log("current user miles left to go " + userMilesToGo);
+    // console.log("current user miles left to go " + userMilesToGo);
     // console.log("current group miles left to go  " + groupMilesToGo);
     
     console.log(formState);
@@ -36,53 +37,45 @@ export default function MileInput(props) {
 
     // const newGroupMiles = parseInt(formState.formMiles) + parseInt(groupMiles);
 
-    const newUserMilesToGo = parseInt(userMilesToGo) - parseInt(formState.formMiles);
+    // const newUserMilesToGo = parseInt(userMilesToGo) - parseInt(formState.formMiles);
 
     // const newGroupMilesToGo = parseInt(groupMilesToGo) - parseInt(formState.formMiles);
 
 
     // console log 
     console.log("updated user miles " + newUserMiles);
+    alert("logged! New total " + newUserMiles)
     // console.log("updated group miles " + newGroupMiles);
-    console.log("updated user miles left to go " + newUserMilesToGo);
+    // console.log("updated user miles left to go " + newUserMilesToGo);
     // console.log("updated group miles left to go " + newGroupMilesToGo)
 
 
     // fetch user id and update userMiles
     // fetch("api/users/:id")
-    // API.updateUser
-    fetch("https://localhost:3001/api/users/journey:id", {
-      method: "post",
-      body: JSON.stringify({
-        userMiles: `${newUserMiles}`,
-        userMilesToGo: `${newUserMilesToGo}`
-      }),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then(function () {
-        console.log("userMiles logged!");
-        console.log("userMilesToGo logged!");
+    // API.logMiles
+    // fetch("https://localhost:3001/api/users/journey:id", {
+    //   method: "post",
+    //   body: JSON.stringify({
+    //     userMiles: `${newUserMiles}`,
+    //     userMilesToGo: `${newUserMilesToGo}`
+    //   }),
+    //   headers: { "Content-Type": "application/json" },
+    // })
+    //   .then(function () {
+    //     console.log("userMiles logged!");
+    //     console.log("userMilesToGo logged!");
+    //   })
+    //   .catch((error) => console.log(error));
+
+    
+      API.logMiles(newUserMiles, props.token).then(res => {
+        console.log(res)
+
       })
-      .catch((error) => console.log(error));
+    
+   
 
 
-
-// // fetch by fellowship id and update groupMiles
-//  // fetch by fellowship id and update groupMilesToGo
-//     // fetch("api/fellowship/:id")
-//     fetch("https://localhost:3001/fellowships/:id",{
-//       method: "post",
-//       body: JSON.stringify({
-//         groupMiles: `${newGroupMiles}`,
-//         groupMilesToGo:`${newGroupMilesToGo}`
-//       }),
-//       headers: {"Content-Type": "application/json"},
-//     })
-//     .then(function () {
-//       console.log("groupMiles logged!");
-//       console.log("groupMilestoGo logged!");
-//     })
-//       .catch((err) => console.log(err));
 
 
      
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,41 +1,43 @@
-const axios = require("axios")
+const axios = require("axios");
 // local
-const URL_PREFIX = "http://localhost:3001"
+const URL_PREFIX = "http://localhost:3001";
 
 // deployed
-// created but nothing pushed to heroku  
+// created but nothing pushed to heroku
 // const URL_PREFIX = "https://there-and-back-end.herokuapp.com/"
 
 const API = {
   login: function (userData) {
-    return axios.post(`${URL_PREFIX}/api/auth`, userData)
+    return axios.post(`${URL_PREFIX}/api/auth`, userData);
   },
   signup: function (userData) {
-    return axios.post(`${URL_PREFIX}/api/users`, userData)
+    return axios.post(`${URL_PREFIX}/api/users`, userData);
   },
   getProfile: function (token, userId) {
     return axios.get(`${URL_PREFIX}/api/users/${userId}`, {
-        headers: {
-            authorization: `bearer ${token}`
-        }
-    })
-},
-  allTrails: function() {
-    return axios.get(`${URL_PREFIX}/api/trails`)
+      headers: {
+        authorization: `bearer ${token}`,
+      },
+    });
+  },
+  allTrails: function () {
+    return axios.get(`${URL_PREFIX}/api/trails`);
   },
 
-  allLotr: function() {
-
-    return axios.get(`${URL_PREFIX}/api/journey/lotr`)
-
+  allLotr: function () {
+    return axios.get(`${URL_PREFIX}/api/journey/lotr`);
   },
 
-  logMiles: function() {
-    return axios.get(`${URL_PREFIX}/api/journey/:id`)
+  logMiles: function (miles, token) {
+    return axios.put(`${URL_PREFIX}/api/users`, {
+     
+      userMiles: miles
+   
+    },{ headers: {
+      authorization: `bearer ${token}`,
+    },}
+    );
+  },
+};
 
-  }
-
-}
-
-export default API
-
+export default API;
